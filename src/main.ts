25358764f3f945534b33467b5ee6e91d4230761e
jhonpulido1990 <3825@holbertonstudents.com>
2024-08-23 15:19:04 -0500

    (add) => [organizacion de cors]

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,12 +12,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-
-  app.enableCors({
-    origin: '*', // Cambia esto por la URL de tu aplicación Angular
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
 
   await app.listen(3000);
 }
